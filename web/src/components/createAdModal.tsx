477ce7f57f015a0b733072f89f688246ef5b4197
Deyvison Iury <iurydeyvison@gmail.com>
2022-09-16 15:35:17 -0300

    Fixed spelling error

--- web/src/components/createAdModal.tsx
+++ web/src/components/createAdModal.tsx
@@ -52,7 +52,7 @@ export function CreateAdModal() {
           <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
 
           <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25" >
-            <Dialog.Title className="text-3xl font-black">Publice um anúncio</Dialog.Title>
+            <Dialog.Title className="text-3xl font-black">Publique um anúncio</Dialog.Title>
 
 
               <form onSubmit={handleCreateAd} className="mt-8 flex flex-col gap-4">
